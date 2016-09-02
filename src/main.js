81d94e3a0484dcf21d3cfdf20e78a65df92160b8
Lsxj <413105383@qq.com>
2016-09-02 19:35:11 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import Vue_Router from 'vue-router'
+import routerMap from './routers.js'
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+
+Vue.use(Vue_Router)
+
+var router = new Vue_Router({
+    hashbang: true,
+    history: false,
+    saveScrollPosition: false,
+    transitionOnLoad: true
 })
+
+// 独立出来的路由
+routerMap(router)
+
+router.start(App, '#puzzleApp')
+
+///* eslint-disable no-new */
+//new Vue({
+//el: 'body',
+//components: { App }
+//})
